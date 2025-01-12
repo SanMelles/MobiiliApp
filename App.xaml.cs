2c363d87c50800efeca038bddc0d7b0d52be7f36
sandermelles@gmail.com <sandermelles@gmail.com>
2025-01-12 20:00:04 +0200

    App window size

--- App.xaml.cs
+++ App.xaml.cs
@@ -7,8 +7,14 @@ namespace MauiAppSolo
         public App()
         {
             InitializeComponent();
-
-            MainPage = new MainPage();
         }
+            protected override Window CreateWindow(IActivationState activationState) =>
+                new Window(new MainPage())
+                {
+                    Width = 600,
+                    Height = 900,
+                    X = 100,
+                    Y = 100
+                };
     }
 }
